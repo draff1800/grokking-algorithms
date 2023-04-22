9b925069dfa2c5ddf945c344dc7958dfdd31c56b
Dylan Rafferty <draff1800@gmail.com>
2023-04-22 15:01:16 -0400

    First commit + Binary Search function

---
+++ BinarySearch.java
@@ -0,0 +1,29 @@
+class BinarySearch {
+
+    public static void main(String[] args) {
+        // Customise binarySearch() call below to experiment with binary search.
+        int[] numberList = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println(binarySearch(numberList, 7));
+    }
+
+    // Accepts ordered list of non-0 numbers and a non-0 number.
+    // If number exists in numberList, number is returned. Otherwise, 0 is returned.
+    public static int binarySearch(int[] numberList, int number) {
+        int lowIndex = 0;
+        int highIndex = numberList.length - 1;
+
+        while (lowIndex <= highIndex) {
+            int midIndex = (lowIndex + highIndex) / 2;
+            int guessedNumber = numberList[midIndex];
+
+            if (guessedNumber < number) {
+                lowIndex = midIndex + 1;
+            } else if (guessedNumber > number) {
+                highIndex = midIndex - 1;
+            } else {
+                return guessedNumber;
+            }
+        }
+        return 0;
+    }
+}
